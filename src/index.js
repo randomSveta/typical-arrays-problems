cdc01a78313379ead620f1282d052436e267d404
Sveta K <test@test.test>
2020-09-29 22:06:17 +0100

    feat: write in min function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 exports.min = function min(array) {
-    // if it is fine to use Math.min(...array) - it's a solution
+    if (Array.isArray(array) && array.length > 0) {
+        // if it is fine to use Math.min(...array) - it's a solution
+        let minNumber = array[0];
+
+        array.forEach(number => {
+            if (number < minNumber) {
+                minNumber = number;
+            }
+        });
+
+        return minNumber;
+    }
     return 0;
 }
 
