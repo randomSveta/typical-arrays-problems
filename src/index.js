f2fc9ce5cbde38561d18968bd7e7bf83cf9fcf19
Sveta K <test@test.test>
2020-09-29 22:03:56 +0100

    init: start "typical arrays problems" task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    // if it is fine to use Math.min(...array) - it's a solution
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    // if it is fine to use Math.max(...array) - it's a solution
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return 0;
 }
